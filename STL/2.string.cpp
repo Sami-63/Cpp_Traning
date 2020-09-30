3e21456d789765f479b463b4fbb335bb84a202be
Rubayet Sadman Sami <69534625+As-Sami@users.noreply.github.com>
2020-09-30 20:31:09 +0600

    Add files via upload

---
+++ STL/2.string.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str = "string 1";
+    str.size();
+    str.length();
+    str.clear();
+    str.empty();
+
+
+
+    return 0;
+}
