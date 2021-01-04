79e9511b50e522684f78570340e399bb17a54027
Alomgir27 <a.h.joy066@gmail.com>
2021-01-05 00:54:39 +0600

    Vector changed

--- STL/1.vector.cpp
+++ STL/1.vector.cpp
@@ -1,38 +1,22 @@
-#include <iostream>
-#include <vector>    ///for vector and their functions
-#include <algorithm> /// sort
+#include <bits/stdc++.h>
 using namespace std;
-
-void showvec(vector<int> &v)
+#define int long long
+int32_t main()
 {
-    for( int i=0 ; i<v.size() ; i++ )
-        cout << v[i] << " \n"[i==v.size()-1];
-    cout << endl;
-}
-int main()
-{
-    vector<int> v1,v2(10),v3(5,-1);
-
-    showvec(v1);
-    showvec(v2);
-    showvec(v3);
-
-    v1.push_back(12);
-    v1.push_back(2);
-    v1.push_back(5);
-    v1.push_back(7);
-    v1.push_back(19);
-    v1.push_back(90);
-    v1.pop_back(); /// delete last element
-
-    v1.insert( v1.begin()+2 , 10); /// pos , val
-    v1.erase( v1.begin()+ 4); /// pos or v1.erase( first pos , last pos)
-    sort( v1.begin() , v1.end() );
-    reverse( v1.begin() , v1.end() );
-
-    v1.empty();
-    v2.clear();
-    v3.size();
-
-    return 0;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        int a[n + 10], ans[n + 10];
+        for(int i = 0;i<n;i++)cin >> a[i];
+        int ans1 = 0;
+        for(int i = n-1;i>=0;i--)
+        {
+            ans[i] = a[i];
+            if(i + a[i] < n)ans[i] += ans[i + a[i]];
+            ans1 = max(ans1, ans[i]);
+        } cout << ans1 << endl;
+    }
 }
